5f6f0796fe9f10e68b28825bbb789a094ba9d07c
SAUCE <3601016@ppti-14-503-11.ufr-info-p6.jussieu.fr>
2017-03-29 09:02:08 +0200

    ajout de index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
+	console.log(message);
+});
+
+client.on('presenceUpdate', function(oldMember, newMember) {
+	console.log(oldMember.presence, '=>', newMember.presence);
+});
+
+client.login(process.env.DISCORD_TOKEN);
