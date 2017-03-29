c7220fe09313becfa5e7f638c593d151e65f0594
SAUCE <3601016@ppti-14-503-11.ufr-info-p6.jussieu.fr>
2017-03-29 10:06:45 +0200

    message

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+/*
 	if (message.mentions.users.get(client.user.id) != undefined) {
 		message.reply('Pas compris');
-	}
-	if(message.isMentioned(client.user.id)){
+	}*/
+	if(message.isMentioned(client.user)){
 		message.reply('Pas compris');
 	}
-	console.log(message);
 });
 
 client.on('presenceUpdate', function(oldMember, newMember) {
 	console.log(oldMember.presence, '=>', newMember.presence);
-	console.log(newMember);
 	if(newMember.presence.status == "online" && newMember.user.username == "Barjow"){
 		newMember.sendMessage("Bonjour maitre, je suis "+client.user.id+" le bot de Ludo et Adel, que puis-je faire pour vous aujourd'hui ?");
 	}
